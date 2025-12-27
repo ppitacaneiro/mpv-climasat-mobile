f5c5dda3841aabd853307414b749289020504d0c
Pablo Pita <ppitacaneiro@gmail.com>
2025-12-27 14:42:46 +0100

    Refactor router configuration to redirect to login page and implement authentication guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,41 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import LoginPage from '../views/LoginPage.vue'
+import JobsPage from '@/views/JobsPage.vue';
+import { useAuthStore } from '@/stores/auth';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/login'
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  }
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: JobsPage
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to) => {
+  const auth = useAuthStore();
+
+  if (to.meta.requiresAuth && !auth.isAuthenticated) {
+    return { name: 'Login' };
+  }
+
+  if (to.name === 'Login' && auth.isAuthenticated) {
+    return { name: 'Jobs' };
+  }
+});
 
 export default router
