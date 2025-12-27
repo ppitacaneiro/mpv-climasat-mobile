2be8abd0429a53daf03779f36aaefeec1035fc73
Pablo Pita <ppitacaneiro@gmail.com>
2025-12-27 14:42:23 +0100

    Add JobsPage component with initial template and script setup

---
+++ src/views/JobsPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <ion-page>
+    <ion-content class="ion-padding">
+      <h1>Trabajos asignados</h1>
+      <p>Listado de trabajos (pendiente)</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonContent } from '@ionic/vue';
+</script>
